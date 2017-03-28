00c1bcdfb54661e9f6afe7860807b73d5bfbac2a
priscillamc <priscillamc@users.noreply.github.com>
2017-03-28 16:47:00 -0400

    Initial commit of base plugin files

---
+++ Readme.md
@@ -0,0 +1,18 @@
+UFCLAS Syllabus Admin
+=====================
+
+WordPress plugin that manages course syllabi. 
+
+Requirements
+------------
+- WordPress
+
+Installation
+------------
+- Install and activate.
+
+Changelog
+---------
+
+To-Do:
+------
---
+++ ufclas-syllabus-admin.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: UFCLAS Syllabus Admin
+Plugin URI: https://it.clas.ufl.edu/
+Description: Admin features to manage course syllabi 
+Version: 0.0.0
+Author: Priscilla Chapman (CLAS IT)
+Author URI: https://it.clas.ufl.edu/
+License: GPL2
+Build Date: 20170328
+*/
